3559742179007088a36c257cda721f5bcf48e69e
Nate Miller <natekmiller96@gmail.com>
2018-07-11 01:40:58 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,14 +1,14 @@
 require 'pry'
 module Players
   class Human < Player
-    def move
+    def move(board)
       moved = false
       while moved == false
         puts "Please select cell."
         input = gets.strip
         input = input.to_i
-        if self.board.valid_move?(input) == true
-          self.board.cells[input] = self.token
+        if board.valid_move?(input) == true
+          board.cells[input] = self.token
           moved = true
         else
           return "invalid"
